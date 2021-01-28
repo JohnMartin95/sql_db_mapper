f99bf78de1de545a8ca79296d6d29e08ed525020
John Martin <john.martin1225@gmail.com>
2021-01-28 15:54:51 -0500

    Provide TryFromRow implementaions for more types

--- sql_db_mapper_core/src/try_from_row.rs
+++ sql_db_mapper_core/src/try_from_row.rs
@@ -29,6 +29,31 @@ macro_rules! try_from_row {
 				row.try_get(0)
 			}
 		}
+		impl TryFromRow for Option<$impl_type> {
+			fn from_row(row: &Row) -> Result<Self, SqlError> {
+				row.try_get(0)
+			}
+		}
+		impl TryFromRow for Vec<$impl_type> {
+			fn from_row(row: &Row) -> Result<Self, SqlError> {
+				row.try_get(0)
+			}
+		}
+		impl TryFromRow for Vec<Option<$impl_type>> {
+			fn from_row(row: &Row) -> Result<Self, SqlError> {
+				row.try_get(0)
+			}
+		}
+		impl TryFromRow for Option<Vec<$impl_type>> {
+			fn from_row(row: &Row) -> Result<Self, SqlError> {
+				row.try_get(0)
+			}
+		}
+		impl TryFromRow for Option<Vec<Option<$impl_type>>> {
+			fn from_row(row: &Row) -> Result<Self, SqlError> {
+				row.try_get(0)
+			}
+		}
 	};
 }
 
@@ -38,6 +63,7 @@ try_from_row!(i16);
 try_from_row!(i32);
 try_from_row!(u32);
 try_from_row!(i64);
+try_from_row!(f32);
 try_from_row!(f64);
 try_from_row!(String);
 try_from_row!(Vec<u8>);
@@ -73,33 +99,54 @@ try_from_tuple!(A, B, C, D, E, F, G, H, I, J, K, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
 try_from_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 
 
+// bit_vec
+#[cfg(feature = "with-bit-vec-0_6")]
+try_from_row!(bit_vec::BitVec);
+
 // chrono
-#[cfg(feature = "chrono")]
-try_from_row!(chrono::NaiveDate);
-#[cfg(feature = "chrono")]
-try_from_row!(chrono::NaiveDateTime);
-#[cfg(feature = "chrono")]
-try_from_row!(chrono::DateTime<chrono::Utc>);
+#[cfg(feature = "with-chrono-0_4")]
+mod chrono_impls {
+    use super::*;
+	try_from_row!(chrono::NaiveDateTime);
+	try_from_row!(chrono::DateTime<chrono::Utc>);
+	try_from_row!(chrono::DateTime<chrono::Local>);
+	try_from_row!(chrono::DateTime<chrono::FixedOffset>);
+	try_from_row!(chrono::NaiveDate);
+	try_from_row!(chrono::NaiveTime);
+}
+
+// eui48
+#[cfg(feature = "with-eui48-0_4")]
+try_from_row!(eui48::MacAddress);
+
+// geo_types
+#[cfg(feature = "geo-types")]
+mod geo_types_impls {
+	use super::*;
+	try_from_row!(geo_types::Point<f64>);
+	try_from_row!(geo_types::Rect<f64>);
+	try_from_row!(geo_types::LineString<f64>);
+}
 
 // rust_decimal
 #[cfg(feature = "rust_decimal")]
 try_from_row!(rust_decimal::Decimal);
 
-// geo_types
-// #[cfg(feature = "geo-types")]
-// try_from_row!(geo_types::Point<f64>);
-// #[cfg(feature = "geo-types")]
-// try_from_row!(geo_types::Rect<f64>);
-// #[cfg(feature = "geo-types")]
-// try_from_row!(geo_types::LineString<f64>);
-
 // serde_json
 #[cfg(feature = "serde_json")]
 try_from_row!(serde_json::Value);
 
+// time
+#[cfg(feature = "with-time-0_2")]
+mod time_impls {
+	use super::*;
+	try_from_row!(time::PrimitiveDateTime);
+	try_from_row!(time::OffsetDateTime);
+	try_from_row!(time::Date);
+	try_from_row!(time::Time);
+
+}
+
 // uuid
 #[cfg(feature = "uuid")]
 try_from_row!(uuid::Uuid);
-// bit_vec
-#[cfg(feature = "bit-vec")]
-try_from_row!(bit_vec::BitVec);
